20f18c5a07f3ebe4bec645493f8c60bcafc3f3ec
friendlyantz <friendlyantz@pm.me>
2023-07-12 21:58:41 +1000

    Notes on Tuples, Heaps and Classes

--- refresher_quirks.py
+++ refresher_quirks.py
@@ -1,4 +1,8 @@
+import heapq
+from collections import deque
+import math
 print("Hello World!")
+
 def print_divider():
     print("----------")
 
@@ -34,7 +38,7 @@ print_divider()
 print("division 5 / 2 and 5 // 2 and int divis")
 print(5 / 2)
 print(5 // 2)
-print( -5 // 2) 
+print(-5 // 2)
 print(int(5 / 2))
 
 print_divider()
@@ -43,7 +47,6 @@ print(10 % 3)
 print("modulo -10 % 3")
 print(-10 % 3)
 
-import math
 
 print_divider()
 print("math.fmod(-10, 3)")
@@ -94,7 +97,8 @@ for letter, num in zip(letters, nums):
 
 print_divider()
 print('sort')
-arr = ['Jan', 'Feb', 'Mar', 'April', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'October', 'Nov', 'Decemb']
+arr = ['Jan', 'Feb', 'Mar', 'April', 'May', 'Jun',
+       'Jul', 'Aug', 'Sep', 'October', 'Nov', 'Decemb']
 arr.sort()
 print(arr)
 arr.sort(key=lambda x: len(x))
@@ -139,7 +143,6 @@ print(' '.join(deez))
 print_divider()
 # queues are DOUBLE ENDED queues
 
-from collections import deque
 
 queue = deque()
 
@@ -178,7 +181,7 @@ hash_set.remove(1)
 print(1 in hash_set)
 
 print(set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-loop_init_hash_set = { i for i in range(7) }
+loop_init_hash_set = {i for i in range(7)}
 print(loop_init_hash_set)
 
 # ==== HASH MAP ====
@@ -188,12 +191,12 @@ hash_map = {
     'a': 1,
     'b': 2,
     'no rocket input': 3
-        }
+}
 print(hash_map)
 
 # ==== HASH Comprehension ====
 
-my_hash_map = { str(i): i * i for i in range(7) }
+my_hash_map = {str(i): i * i for i in range(7)}
 print(my_hash_map)
 
 for value in my_hash_map.values():
@@ -201,3 +204,110 @@ for value in my_hash_map.values():
 
 for key, value in my_hash_map.items():
     print(key, value)
+
+# ==== TUPLES ====
+
+print_divider()
+print('Tuples - are immutable')
+t = (1, 2, 3)
+print(t)
+print(t[0])
+print(t[1:])
+print(t + (4, 5))
+print(t * 3)
+print(t)
+
+
+print('tuples can be used as keys in hash maps unlike lists')
+
+# ==== HEAP ====
+# under the hood, heaps are implemented as arrays
+# by default, they are MIN heaps, for MAX heaps, multiply by -1 when pushing and popping
+print_divider()
+
+
+min_heap = []
+heapq.heappush(min_heap, 5)
+heapq.heappush(min_heap, 2)
+heapq.heappush(min_heap, 3)
+
+print(min_heap[0])
+
+while min_heap:
+    print(heapq.heappop(min_heap))
+
+
+print_divider()
+print('max heap workaround')
+max_heap = []
+heapq.heappush(max_heap, -5)
+heapq.heappush(max_heap, -2)
+heapq.heappush(max_heap, -3)
+
+print(-max_heap[0])
+
+
+print_divider()
+print('heapify')
+arr = [5, 2, 3, 1, 4]
+heapq.heapify(arr)
+print(arr)
+print(arr[0])
+
+# ==== FUNCTIONS ====
+print_divider()
+print('functions')
+
+
+def add(x, y):
+    return x + y
+
+
+def classy_func(arr, var):
+    c = 'cccc'
+
+    def inner_func():
+        # variables in the outer scope can be accessed in the inner scope
+        arr[0] = 'changed'
+        print('inner func')
+        print(c)
+        print(arr)
+
+        # nonlocal keyword allows you to modify variables in the outer scope
+        nonlocal var
+        var *= 2
+        print(var)
+
+    inner_func()
+
+
+classy_func([1, 2, 3], 7)
+
+# ==== CLASSES ====
+
+print_divider()
+print('classes')
+
+
+class Dog:
+    # self is the instance of the class
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []
+
+    # always pass self as the first argument
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+    def add_default_tricks(self):
+        self.add_trick('slurp')
+
+
+doggo = Dog('Fido')
+doggo.add_default_tricks()
+print(doggo.tricks)
+doggo.add_trick('roll over')
+doggo.add_trick('pass my computer science challenge')
+
+print(doggo.name)
+print(doggo.tricks)
